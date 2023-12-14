7e672d4f05d686a9cc2bbfbd5092431d19364489
DavidKopec <147917337+DavidKopec@users.noreply.github.com>
2023-12-14 13:53:06 +0100

    Add files via upload

--- CG_LongRun/CreationToSim_LLCG.py
+++ CG_LongRun/CreationToSim_LLCG.py
@@ -9,7 +9,7 @@ PROT = "GLY ALA VAL CYS PRO LEU ILE MET TRP PHE LYS ARG HIS SER THR TYR ASN GLN 
 
 def prepare_peptides(count:int) ->None:
     coords = [0,0,0] # coordinates on start
-    shifts = [[0,0],[1.2,0],[0,1.2],[1.2,1.2],[1.2,-1.2]] # shifts we want to use
+    shifts = [[0,0],[2,3.5],[0,3.5],[2,2],[2,-2]] # shifts we want to use
     
     peptide_num = 0 # how many peptides were generated already
     x,y,_ = coords
@@ -30,7 +30,6 @@ def prepare_peptides(count:int) ->None:
                     break
             if(peptide_num==count):break
 
-# need to add the topology editation part !!!!
 def assemble_system(count:int):
 
     fill=""
@@ -54,11 +53,21 @@ def assemble_system(count:int):
         f.write(fill)
     os.system(f"""module add {GROMACS_V}
               gmx editconf -f sys_gen.pdb -o system.gro""")
+    
+    with open("system.top","r") as f:
+        fill=""
+        for line in f.readlines():
+            if("[ molecules ]" in line):
+                fill+= line + "\nProtein_A\t" + str(count)
+                continue
+            fill+=line
+    with open("system.top","w") as f:
+        f.write(fill)
     pass
 
 def ionize():
     os.system(f"cp {DEPENDENCIES}/ions.mdp")
-    os.system(f"""module add {GROMACS_V}
+    os.system(f"""module add {GROMACS_V} 
               gmx grompp -f ions.mdp -c system.gro -p system.top -o ions.tpr
               gmx genion -s ions.tpr -pname NA -nname CL -neutral -o system_ion.gro -p system.top""")
     
@@ -69,8 +78,8 @@ def index_file():
     membrane = u.select_atoms("resname DOPC POPS DOPE")
     water = u.select_atoms("resname SOL or resname W")
     ions = u.select_atoms("name NA CL CLA SOD or resname ION")
-    SOLU = u.select_atoms(f"resname POPS DOPC DOPE or {PROT}")
-    SOLV = u.select_atoms("name CL NA SOL SOD CLA W")
+    SOLU = u.select_atoms(f"resname POPS DOPC DOPE or resname {PROT}")
+    SOLV = u.select_atoms("name CL NA SOL SOD CLA W PW or resname ION")
     
     with mda.selections.gromacs.SelectionWriter("index.ndx", mode="w") as ndx:
         ndx.write(membrane, name="Membrane")
@@ -83,23 +92,23 @@ def index_file():
 def std_sim():
     def minim():
         os.system("mkdir MIN")
-        os.system("cp system_ion.gro index.ndx system.top ./MIN")
+        os.system("cp -r system_ion.gro index.ndx system.top FF ./MIN")
         os.system(f"cp {DEPENDENCIES}/minim*.mdp ./MIN")
         os.chdir(os.getcwd()+"/MIN")
         os.system(f"""module add {GROMACS_V}
-                   gmx grompp -f minim1.mdp -c system_ion.gro -n index.ndx -p system.top -o minim1.gro -maxwarn 1
+                   gmx grompp -f minim1.mdp -c system_ion.gro -r system_ion.gro -n index.ndx -p system.top -o minim1.tpr -maxwarn 1
                    gmx mdrun -deffnm minim1 -v
-                   gmx grompp -f minim2.mdp -c minim1.gro -n index.ndx -p system.top -o minim2.gro -maxwarn 1
+                   gmx grompp -f minim2.mdp -c minim1.gro -r minim1.gro -n index.ndx -p system.top -o minim2.tpr -maxwarn 1
                    gmx mdrun -deffnm minim2 -v""")
-        os.chdir(os.getcwd()+"/..")
     def sim():
+        if(os.getcwd().split("/")[-1]=="MIN"):
+            os.chdir(os.getcwd()+"/..")
         os.system("mkdir SIM")
-        os.system("cp minim2.gro index.ndx system.top ./SIM")
+        os.system("cp -r FF MIN/minim2.gro index.ndx system.top ./SIM")
         os.system(f"cp {DEPENDENCIES}/*.mdp ./SIM")
         os.system(f"cp {DEPENDENCIES}/SIM ./SIM")
         os.chdir(os.getcwd()+"/SIM")
-        os.system("psubmit cpu SIM ncpus=8 walltime=1d -y")
-        os.chdir(os.getcwd()+"/..")
+        os.system("psubmit gpu SIM ncpus=8, ngpus=1 walltime=1d -y")
 
     minim()
     sim()
@@ -109,11 +118,11 @@ def main():
         print("Usage: python3 script.py peptide_count")
         sys.exit()
     
-    #prepare_peptides(int(sys.argv[1]))
-    #assemble_system(int(sys.argv[1]))
+    prepare_peptides(int(sys.argv[1]))
+    assemble_system(int(sys.argv[1]))
     ionize()
-    #index_file()
-    #std_sim()
+    index_file()
+    std_sim()
     
     pass
 
