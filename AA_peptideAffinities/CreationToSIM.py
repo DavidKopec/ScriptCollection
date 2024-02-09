d964930c94e09ea76ac2df60e8eb90eaec0c83cd
DavidKopec <147917337+DavidKopec@users.noreply.github.com>
2024-02-09 09:39:26 +0100

    Update CreationToSIM.py

--- AA_peptideAffinities/CreationToSIM.py
+++ AA_peptideAffinities/CreationToSIM.py
@@ -8,6 +8,7 @@ SYSTEM:str="system.pdb"
 PEPTIDE:str ="MP1_rdy.pdb"
 TOPOLOGY:str = "system.top"
 INDEX:str = "index.ndx"
+MAIN_DIR:str = ''
 
 def create_system():
     peptide_pdb:str=""
@@ -32,10 +33,12 @@ def create_system():
         f.write(system_pdb)
     
     os.system(f"""module add {GROMACS_V}
-                gmx editconf -f {SYSTEM} -system.gro""")
+                gmx editconf -f {SYSTEM} -o system.gro""")
     
 
 def ionization():
+    os.chdir(MAIN_DIR)
+
     os.system("mkdir 01_IONIZATION")
     os.system("cp -r {system.gro,system.top,index.ndx,FF} ./01_IONIZATION")
     os.system(f"cp -r {PATH_DEP}/ion.mdp ./01_IONIZATION")
@@ -43,12 +46,11 @@ def ionization():
     os.chdir("./01_IONIZATION")
 
     os.system(f"""module add {GROMACS_V}
-                gmx grompp -f ion.mdp -c system.gro -p system.top -o ion.tpr
+                gmx grompp -f ion.mdp -c system.gro -p system.top -o ion.tpr -maxwarn 1
                 gmx genion -s ion.tpr -pname NA -nname CL -neutral -p system.top -o system.gro""")
 
 def simulation():
-    if(os.getcwd().split("/")[-1] =="01_IONIZATION"):
-        os.chdir(os.getcwd()+"../")
+    os.chdir(MAIN_DIR)
     
     os.system("mdkir 02_MINIMIZATION")
     os.system("cp -r ./01_IONIZATION/{system.gro,system.top,FF} ./02_MINIMIZATION")
@@ -60,12 +62,10 @@ def simulation():
     u = mda.Universe("system.gro")
 
     membrane = u.select_atoms("resname DOPC POPS DOPE")
-    #pumed_selec = u.select_atoms("(resname DOPC POPS DOPE) and (name C1A D2A C3A C4A C1B C2B C3B C4B)")
     water = u.select_atoms("resname PW")
     ions = u.select_atoms("name NA CL")
-    SOLU = u.select_atoms("resname POPS DOPC DOPE")
+    SOLU = u.select_atoms("resname POPS DOPC DOPE GLY ALA VAL CYS PRO LEU ILE MET TRP PHE LYS ARG HIS SER THR TYR ASN GLN ASP GLU") # Myabe select also the protein ?
     SOLV = u.select_atoms("name CL NA or resname PW")
-    SOLV2 = u.select_atoms("name CL NA or resname PW")
     
     with mda.selections.gromacs.SelectionWriter("index.ndx", mode="w") as ndx:
         ndx.write(membrane, name="Membrane")
@@ -74,16 +74,25 @@ def simulation():
         ndx.write(ions, name="Ions")
         ndx.write(SOLU, name="MEMB_PROT")
         ndx.write(SOLV, name="W_ION")
-        ndx.write(SOLV2, name="W_ION")  
 
     os.system(f"""module add {GROMACS_V}
             gmx grompp -f minim.mdp -c system.gro -p system.top -r system.gro -n index.ndx -o minim.tpr
             gmx mdrun -deffnm minim -v""")
+    
+    os.chdir(MAIN_DIR)
+    os.system('mdkir 03_SIM')
+    os.system("cp -r ./02_MINIMIZATION/{minim.gro,system.top,FF} ./03_SIM")
+    os.system(f"cp -r {PATH_DEP}/"+"{*.mdp,SIM}" +" ./03_SIM")
+
+    os.system('psubmit gpu SIM ncpus=16 ngpus=2 walltime=1d -y')
+
 
     pass
 
 
 def main():
+    global MAIN_DIR
+    MAIN_DIR = os.getcwd()
     pass
 
 if __name__=="__main__":
